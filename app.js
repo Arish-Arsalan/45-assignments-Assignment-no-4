68951d592fd801c2479eabeb5807d66960c87d27
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-03-13 21:31:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new";
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, ${quote}`;
+console.log(message);
